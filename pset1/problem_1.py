f52e2e918ebd0e2d7e293cbe8c1cae89acf1b737
Tom Wagg <twagg@college.harvard.edu>
2022-01-14 08:46:36 -0800

    make it fit on the screen

--- pset1/problem_1.py
+++ pset1/problem_1.py
@@ -3,7 +3,7 @@ from simulator import Simulation
 import matplotlib.pyplot as plt
 
 # create a new class for the simulation with some randomish variable choices
-sim = Simulation(N=100, E=0.00001, size=1000, rad=3, delay=200)
+sim = Simulation(N=100, E=10, size=750, rad=3, delay=200)
 
 # run the simulation
 sim.run_simulation(steps=10)
